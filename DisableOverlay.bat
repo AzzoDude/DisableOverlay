1dcb644103a72c5b486de4aed7ba1e50b01c38b7
Nguyễn Quý Ngọc <76089776+AzzoDude@users.noreply.github.com>
2026-04-21 14:46:22 +0700

    Update DisableOverlay.bat

--- DisableOverlay.bat
+++ DisableOverlay.bat
@@ -1,4 +1,12 @@
 @echo off
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Error: Please run this script as an Administrator.
+    pause
+    exit /b
+)
+
 title Game Bar Removal Tool
 echo --------------------------------------------------
 echo Disabling Windows Gaming Overlay...
@@ -6,21 +14,11 @@ echo --------------------------------------------------
 
 :: 1. Disable Game DVR and Game Bar in Registry
 echo Modifying Registry keys...
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "HistoricalCaptureEnabled" /t REG_DWORD /d 0 /f
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v "AllowGameDVR" /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d 0 /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "HistoricalCaptureEnabled" /t REG_DWORD /d 0 /f >nul
+reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d 0 /f >nul
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v "AllowGameDVR" /t REG_DWORD /d 0 /f >nul
 
-:: 2. Stop and Disable the Gaming Services (if running)
+:: 2. Stop and Disable Gaming Services
 echo Stopping background services...
-powershell -Command "Get-Service -Name GamingServices* | Stop-Service -Force" >nul 2>&1
-
-:: 3. Uninstall the Game Bar AppX Package
-echo Removing AppX Packages...
-powershell -Command "Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage"
-powershell -Command "Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like '*XboxGamingOverlay*'} | Remove-AppxProvisionedPackage -Online"
-
-echo --------------------------------------------------
-echo Done! Please restart your computer for changes to take effect.
-echo --------------------------------------------------
-pause
+powershell -Command "Get-Service -Name 'GamingServices*' -ErrorAction SilentlyContinue | Stop-Service -Force" >
